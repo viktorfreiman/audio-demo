c716c00d8e07770bec150085cc5f6f23edc942e2
Viktor Freiman <freiman.viktor@gmail.com>
2023-11-18 22:38:35 +0100

    demo to send single mtc frame

---
+++ loop.js
@@ -0,0 +1,203 @@
+/*
+5.756 L1: type: 0, value: 7, _type: mtc
+5.764 L1: type: 1, value: 0, _type: mtc
+5.772 L1: type: 2, value: 6, _type: mtc
+5.780 L1: type: 3, value: 0, _type: mtc
+5.790 L1: type: 4, value: 0, _type: mtc
+5.791 L1: _type: clock
+5.798 L1: type: 5, value: 0, _type: mtc
+5.806 L1: type: 6, value: 0, _type: mtc
+5.814 L1: type: 7, value: 6, _type: mtc
+5.822 L1: type: 0, value: 9, _type: mtc
+5.832 L1: type: 1, value: 0, _type: mtc
+5.832 L1: _type: clock
+5.840 L1: type: 2, value: 6, _type: mtc
+5.848 L1: type: 3, value: 0, _type: mtc
+5.855 L1: type: 4, value: 0, _type: mtc
+5.863 L1: type: 5, value: 0, _type: mtc
+5.873 L1: type: 6, value: 0, _type: mtc
+5.873 L1: _type: clock
+5.881 L1: type: 7, value: 6, _type: mtc
+5.889 L1: type: 0, value: 11, _type: mtc
+5.897 L1: type: 1, value: 0, _type: mtc
+5.906 L1: type: 2, value: 6, _type: mtc
+5.914 L1: type: 3, value: 0, _type: mtc
+5.915 L1: _type: clock
+5.922 L1: type: 4, value: 0, _type: mtc
+5.930 L1: type: 5, value: 0, _type: mtc
+5.938 L1: type: 6, value: 0, _type: mtc
+5.948 L1: type: 7, value: 6, _type: mtc
+5.956 L1: type: 0, value: 13, _type: mtc
+5.956 L1: _type: clock
+5.964 L1: type: 1, value: 0, _type: mtc
+5.973 L1: type: 2, value: 6, _type: mtc
+5.981 L1: type: 3, value: 0, _type: mtc
+5.989 L1: type: 4, value: 0, _type: mtc
+5.997 L1: type: 5, value: 0, _type: mtc
+5.997 L1: _type: clock
+6.6 L1: type: 6, value: 0, _type: mtc
+6.14 L1: type: 7, value: 6, _type: mtc
+6.22 L1: type: 0, value: 15, _type: mtc
+6.30 L1: type: 1, value: 0, _type: mtc
+6.40 L1: type: 2, value: 6, _type: mtc
+6.40 L1: _type: clock
+6.48 L1: type: 3, value: 0, _type: mtc
+6.56 L1: type: 4, value: 0, _type: mtc
+6.64 L1: type: 5, value: 0, _type: mtc
+6.73 L1: type: 6, value: 0, _type: mtc
+6.81 L1: type: 7, value: 6, _type: mtc
+6.82 L1: _type: clock
+6.89 L1: type: 0, value: 1, _type: mtc
+6.97 L1: type: 1, value: 1, _type: mtc
+6.105 L1: type: 2, value: 6, _type: mtc
+6.115 L1: type: 3, value: 0, _type: mtc
+6.123 L1: type: 4, value: 0, _type: mtc
+6.123 L1: _type: clock
+6.131 L1: type: 5, value: 0, _type: mtc
+6.139 L1: type: 6, value: 0, _type: mtc
+6.147 L1: type: 7, value: 6, _type: mtc
+6.157 L1: type: 0, value: 3, _type: mtc
+6.164 L1: type: 1, value: 1, _type: mtc
+6.164 L1: _type: clock
+6.172 L1: type: 2, value: 6, _type: mtc
+6.182 L1: type: 3, value: 0, _type: mtc
+6.190 L1: type: 4, value: 0, _type: mtc
+6.198 L1: type: 5, value: 0, _type: mtc
+6.206 L1: type: 6, value: 0, _type: mtc
+6.206 L1: _type: clock
+6.215 L1: type: 7, value: 6, _type: mtc
+6.223 L1: type: 0, value: 5, _type: mtc
+6.231 L1: type: 1, value: 1, _type: mtc
+6.239 L1: type: 2, value: 6, _type: mtc
+6.247 L1: type: 3, value: 0, _type: mtc
+6.247 L1: _type: clock
+6.255 L1: type: 4, value: 0, _type: mtc
+6.265 L1: type: 5, value: 0, _type: mtc
+6.272 L1: type: 6, value: 0, _type: mtc
+6.280 L1: type: 7, value: 6, _type: mtc
+6.289 L1: type: 0, value: 7, _type: mtc
+6.289 L1: _type: clock
+6.297 L1: type: 1, value: 1, _type: mtc
+6.305 L1: type: 2, value: 6, _type: mtc
+6.315 L1: type: 3, value: 0, _type: mtc
+6.323 L1: type: 4, value: 0, _type: mtc
+6.331 L1: type: 5, value: 0, _type: mtc
+6.331 L1: _type: clock
+6.338 L1: type: 6, value: 0, _type: mtc
+6.348 L1: type: 7, value: 6, _type: mtc
+6.356 L1: type: 0, value: 9, _type: mtc
+6.363 L1: type: 1, value: 1, _type: mtc
+6.373 L1: type: 2, value: 6, _type: mtc
+6.373 L1: _type: clock
+6.381 L1: type: 3, value: 0, _type: mtc
+6.389 L1: type: 4, value: 0, _type: mtc
+6.397 L1: type: 5, value: 0, _type: mtc
+6.407 L1: type: 6, value: 0, _type: mtc
+6.415 L1: type: 7, value: 6, _type: mtc
+6.415 L1: _type: clock
+6.422 L1: type: 0, value: 11, _type: mtc
+6.431 L1: type: 1, value: 1, _type: mtc
+6.438 L1: type: 2, value: 6, _type: mtc
+6.447 L1: type: 3, value: 0, _type: mtc
+6.455 L1: type: 4, value: 0, _type: mtc
+6.456 L1: _type: clock
+6.463 L1: type: 5, value: 0, _type: mtc
+6.473 L1: type: 6, value: 0, _type: mtc
+6.481 L1: type: 7, value: 6, _type: mtc
+6.489 L1: type: 0, value: 13, _type: mtc
+6.497 L1: type: 1, value: 1, _type: mtc
+6.497 L1: _type: clock
+6.507 L1: type: 2, value: 6, _type: mtc
+6.514 L1: type: 3, value: 0, _type: mtc
+6.522 L1: type: 4, value: 0, _type: mtc
+6.530 L1: type: 5, value: 0, _type: mtc
+6.538 L1: type: 6, value: 0, _type: mtc
+6.539 L1: _type: clock
+6.548 L1: type: 7, value: 6, _type: mtc
+6.556 L1: type: 0, value: 0, _type: mtc
+6.564 L1: type: 1, value: 0, _type: mtc
+6.573 L1: type: 2, value: 7, _type: mtc
+6.581 L1: type: 3, value: 0, _type: mtc
+6.581 L1: _type: clock
+6.589 L1: type: 4, value: 0, _type: mtc
+6.597 L1: type: 5, value: 0, _type: mtc
+6.606 L1: type: 6, value: 0, _type: mtc
+6.614 L1: type: 7, value: 6, _type: mtc
+6.622 L1: type: 0, value: 2, _type: mtc
+6.622 L1: _type: clock
+*/
+
+/*
+Value          description
+  0	   Current Frames Low Nibble
+  1	   Current Frames High Nibble
+  2	   Current Seconds Low Nibble
+  3	   Current Seconds High Nibble
+  4	   Current Minutes Low Nibble
+  5	   Current Minutes High Nibble
+  6	   Current Hours Low Nibble
+  7	   Current Hours High Nibble and SMPTE Type
+*/
+
+data = [
+    // { "type": 0, "value": 6 },
+    // { "type": 1, "value": 0 },
+    // { "type": 2, "value": 10 },
+    // { "type": 3, "value": 0 },
+    // // new frame, 40ms
+    // { "type": 4, "value": 0 },
+    // { "type": 5, "value": 0 },
+    // { "type": 6, "value": 0 },
+    // { "type": 7, "value": 6 },
+    // // new frame, 40ms
+    { "type": 0, "value": 14 },
+    { "type": 1, "value": 0 },
+    { "type": 2, "value": 2 },
+    { "type": 3, "value": 1 },
+    // new frame, 40ms
+    { "type": 4, "value": 2 },
+    { "type": 5, "value": 2 },
+    { "type": 6, "value": 12 },
+    { "type": 7, "value": 6 },
+    // new frame, 40ms
+    // { "type": 0, "value": 8 },
+    // { "type": 1, "value": 0 },
+    // { "type": 2, "value": 10 },
+    // { "type": 3, "value": 0 },
+    // { "type": 4, "value": 0 },
+    // { "type": 5, "value": 0 },
+    // { "type": 6, "value": 0 },
+    // { "type": 7, "value": 6 },
+    // { "type": 0, "value": 12 },
+    // { "type": 1, "value": 0 },
+    // { "type": 2, "value": 10 },
+    // { "type": 3, "value": 0 },
+    // { "type": 4, "value": 0 },
+    // { "type": 5, "value": 0 },
+    // { "type": 6, "value": 0 },
+    // { "type": 7, "value": 6 },
+];
+
+const framerate = 25;
+const msframe = 1000 / framerate;
+const qframe = msframe / 4;
+
+const easymidi = require('easymidi');
+
+console.log('Connecting to: L1');
+const output = new easymidi.Output("L1");
+
+setInterval(() => {
+
+    let i = 0;
+    const interval = setInterval(() => {
+        if (i < data.length) {
+            console.log(data[i]);
+            output.send('mtc', data[i]);
+            i++;
+        } else {
+            clearInterval(interval);
+        }
+    }, 10);
+
+}, data.length * 10);
